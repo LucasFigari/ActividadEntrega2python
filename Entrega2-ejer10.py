98db5bc28b0b284883aba6867a52a2c62c3b25de
LucasFigari <figarilucas4@gmail.com>
2023-04-10 21:07:47 -0300

    Arreglo en el pasaje de parametros de la funcion. Se agregó ademas un while para corroborar que el enlace de alumno y nota se hizo correctamente.

--- Entrega2-ejer10.py
+++ Entrega2-ejer10.py
@@ -4,7 +4,7 @@ from collections import Counter
 nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR',
 'David', 'Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo',
 'Francsica', 'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan',
-'Joaquina', 'Jorge','JOSE', 'Javier', 'Joaquín' , 'Julian', 'Julieta', 'Luciana',
+'Joaquina', 'Jorge', 'JOSE', 'Javier', 'Joaquín' , 'Julian', 'Julieta', 'Luciana',
 'LAUTARO', 'Leonel', 'Luisa', 'Luis', 'Marcos', 'María', 'MATEO', 'Matias',
 'Nicolás', 'Nancy', 'Noelia', 'Pablo', 'Priscila', 'Sabrina', 'Tomás', 'Ulises',
 'Yanina' '''
@@ -17,11 +17,33 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 
 #Instancio una variable de tipo diccionario para ir uniendo según el alumno, las notas
 alumnos = {}
-def enlazarAlumnoNota(nom, puntaje1, puntaje2, **dic):
+def enlazarAlumnoNota(nom, puntaje1, puntaje2, dic):
   """funcion que agrega al diccionario de alumnos, el nombre como clave, y las notas como diccionario dentro del alumno para tener 2 valores organizados"""
   dic[nom] = {"nota1": puntaje1,"nota2": puntaje2}
 #FIN
 
-contador = len(nombres)
-for iterador in contador:
-  enlazarAlumnoNota(nombres[iterador],notas_1[iterador],notas_2[iterador],alumnos)
+nombres2 = nombres.lower().replace(",","").replace("'"," ").split()
+
+contador = len(nombres2)  #Inicia el for, el cual se repite la misma cantidad que alumnos haya.
+print(contador)
+for iterador in range(0,contador):
+  auxnom = nombres2[iterador]
+  auxn1 = notas_1[iterador]
+  auxn2 = notas_2[iterador]
+  enlazarAlumnoNota(nombres2[iterador], notas_1[iterador], notas_2[iterador], alumnos)
+#finaliza el for
+
+
+alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
+while (alumno_ingresado != "fin"):
+  alumno_ingresado = alumno_ingresado.lower()
+  cant_notas = input("Ingrese 'T' para ver todas las notas/Ingrese nota1 para ver la nota 1/Ingrese nota2 para ver la nota 2: ")
+  cant_notas = cant_notas.lower().replace(" ","")
+  match cant_notas:
+    case "t":
+      print(f"el alumno {alumno_ingresado} tiene {alumnos[alumno_ingresado]}")
+    case "nota1":
+      print(f"el alumno {alumno_ingresado} tiene en nota 1: {alumnos[alumno_ingresado]['nota1']}")
+    case "nota2":
+      print(f"el alumno {alumno_ingresado} tiene en nota 2: {alumnos[alumno_ingresado]['nota2']}")
+  alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
