22a11fe244e0125fa10644d83e4fc6d63bf320c8
LucasFigari <figarilucas4@gmail.com>
2023-04-13 22:51:22 -0300

    Se arregló la funcion que calcula el promedio de cada alumno.

--- Entrega2-ejer10.py
+++ Entrega2-ejer10.py
@@ -17,33 +17,78 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 
 #Instancio una variable de tipo diccionario para ir uniendo según el alumno, las notas
 alumnos = {}
+
+# SECCION DE FUNCIONES: COMIENZO
+
 def enlazarAlumnoNota(nom, puntaje1, puntaje2, dic):
   """funcion que agrega al diccionario de alumnos, el nombre como clave, y las notas como diccionario dentro del alumno para tener 2 valores organizados"""
-  dic[nom] = {"nota1": puntaje1,"nota2": puntaje2}
-#FIN
+  dic[nom] = {"nota1": puntaje1, "nota2": puntaje2}
+
+#------------------------------------------------------------------------------------------------------------
+
+def corroborarAlumno(dic):
+  """FUNCIÓN QUE SOLICITA UN ALUMNO E IMPRIME LA/S NOTA/S CORRESPONDIENTES"""
+  alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
+  while (alumno_ingresado != "fin"):
+    alumno_ingresado = alumno_ingresado.lower()
+    cant_notas = input("Ingrese 'T' para ver todas las notas/Ingrese nota1 para ver la nota 1/Ingrese nota2 para ver la nota 2: ")
+    cant_notas = cant_notas.lower().replace(" ","")
+    match cant_notas:
+      case "t":
+        print(f"el alumno {alumno_ingresado} tiene {alumnos[alumno_ingresado]}")
+      case "nota1":
+        print(f"el alumno {alumno_ingresado} tiene en nota 1: {alumnos[alumno_ingresado]['nota1']}")
+      case "nota2":
+        print(f"el alumno {alumno_ingresado} tiene en nota 2: {alumnos[alumno_ingresado]['nota2']}")
+    alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
+
+#------------------------------------------------------------------------------------------------------------
+
+def promedioAlumno(n1,n2,nom):
+  """CALCULA PROMEDIO DE ALUMNO"""
+  suma = n1 + n2
+  promedio = suma/2
+  print(f"el promedio del alumno {nom} es: {promedio}")
+
+#------------------------------------------------------------------------------------------------------------
+
+def sumarnotas(n1,n2):
+  """SUMA 2 NOTAS DEL ALUMNO Y RETORNA EL TOTAL SUMADO"""
+  sum = n1 + n2
+  return sum
+
+#------------------------------------------------------------------------------------------------------------
+
+def promedioDelCurso(alumn,cantalumnos): #el promedio del curso lo voy a calcular en base a esta definicion:El promedio general (GPA) se obtiene dividiendo el número total de calificaciones obtenidas por la cantidad total de horas cursadas.
+  """CALCULA EL PROMEDIO GENERAL DEL CURSO"""
+  total = 0
+  for iter in range(0,cantalumnos):
+     total = total + sumarnotas(alumn[iter]['nota1'],alumn[iter]['nota2'])
+  prom = total/200  #asumo que la cantidad de horas cursadas son 200hs
+  print(f"el promedio general del curso es de {prom}")
+
+#------------------------------------------------------------------------------------------------------------
+
+def promedioDeCadaAlumno(alu):
+  """FUNCIÓN QUE CALCULA EL PROMEDIO DE CADA ALUMNO Y LO IMPRIME EN BASE A UN DICCIONARIO"""
+  for nom, valor in alu.items():
+    promedioAlumno(valor['nota1'],valor['nota2'], nom)
+  
+#------------------------------------------------------------------------------------------------------------
+
+#SECCIÓN DE FUNCIONES: FINAL
 
 nombres2 = nombres.lower().replace(",","").replace("'"," ").split()
-
 contador = len(nombres2)  #Inicia el for, el cual se repite la misma cantidad que alumnos haya.
-print(contador)
+
 for iterador in range(0,contador):
-  auxnom = nombres2[iterador]
-  auxn1 = notas_1[iterador]
-  auxn2 = notas_2[iterador]
   enlazarAlumnoNota(nombres2[iterador], notas_1[iterador], notas_2[iterador], alumnos)
 #finaliza el for
 
 
-alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
-while (alumno_ingresado != "fin"):
-  alumno_ingresado = alumno_ingresado.lower()
-  cant_notas = input("Ingrese 'T' para ver todas las notas/Ingrese nota1 para ver la nota 1/Ingrese nota2 para ver la nota 2: ")
-  cant_notas = cant_notas.lower().replace(" ","")
-  match cant_notas:
-    case "t":
-      print(f"el alumno {alumno_ingresado} tiene {alumnos[alumno_ingresado]}")
-    case "nota1":
-      print(f"el alumno {alumno_ingresado} tiene en nota 1: {alumnos[alumno_ingresado]['nota1']}")
-    case "nota2":
-      print(f"el alumno {alumno_ingresado} tiene en nota 2: {alumnos[alumno_ingresado]['nota2']}")
-  alumno_ingresado = input("ingrese un alumno(fin para finalizar): ")
+corroborarAlumno(alumnos) #no es para ningun item en especial, solo para comprobar que se enlazó bien los nombres y notas.
+
+#ITEM B
+promedioDeCadaAlumno(alumnos)
+
+#ITEM C
